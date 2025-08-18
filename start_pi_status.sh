50dcc338f5ab66b7b279e03be69b335e108c8d5c
Taylor Cohron <jcohron@anl.gov>
2025-08-18 09:16:58 -0500

    changing to default pi username for main branch.

--- start_pi_status.sh
+++ start_pi_status.sh
@@ -6,9 +6,9 @@
 
 # IMPORTANT: Replace 'pi' with your actual username if it's different.
 # This assumes your repository is cloned to /home/pi/pi_slack_status.
-REPO_PATH="/home/ansible/Projects/pi_slack_status"
-VENV_PATH="/home/ansible/.venv/pi-slack"
-USER_NAME="ansible"
+REPO_PATH="/home/pi/Projects/pi_slack_status"
+VENV_PATH="/home/pi/.venv/pi-slack"
+USER_NAME="pi"
 
 # Navigate to the repository directory.
 cd "$REPO_PATH" || { echo "Failed to change directory to $REPO_PATH"; exit 1; }
